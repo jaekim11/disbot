3e3ee2b4eb90ea337bf71fc8f430fb97fd36c6e5
jaekim11 <66285984+jaekim11@users.noreply.github.com>
2021-05-24 13:15:03 +0900

    Update seo.py

--- seo.py
+++ seo.py
@@ -133,9 +133,15 @@ class chatbot(discord.Client):
         if message.content.startswith("!클레"):
             embed=discord.Embed(title="클레", description="클레 가이드", color=0xCC0000)
             embed.set_thumbnail(url="https://ac.namu.la/20210430/f640d2accbd89a2092380944a6ed4cbaa843e4ccf6db04c586fd645e5fe0ffde.png")
-            embed.add_field(name="클레",value='https://arca.live/b/genshin/24956371', inline=False)
-            await message.channel.send(embed=embed)                      
-
+            embed.add_field(name="클레 관련 정보",value='https://arca.live/b/genshin/24956371', inline=False)
+            await message.channel.send(embed=embed)
+            
+        if message.content.startswith("!호두"):
+            embed=discord.Embed(title="호두", description="호두 가이드", color=0xCC0000)
+            embed.set_thumbnail(url="https://ac.namu.la/20210430/3ea68dfbe2fc1b640db93fcc40a1646cfa4ab0abff25f6526b54bb8f25140696.png")
+            embed.add_field(name="호두 관련 ",value='https://arca.live/b/genshin/24956857', inline=False)
+            await message.channel.send(embed=embed)
+            
         if message.content.startswith('!롤'):
             channel = message.channel
             try:
